eb592293881741e236f275618b6bb82ceb4b8a4d
Maxwell Alpha <160728616+mxwlllph@users.noreply.github.com>
2025-07-26 16:46:07 +0800

    Create DigiFlazzApiException.php

---
+++ src/Exceptions/DigiFlazzApiException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mxwlllph\MaxDigi\Exceptions;
+
+use Exception;
+
+/**
+ * Exception yang dilempar ketika terjadi kegagalan saat berkomunikasi
+ * dengan API DigiFlazz.
+ *
+ * @author Maxwell Alpha
+ */
+class DigiFlazzApiException extends Exception
+{
+    // Anda bisa menambahkan konteks atau properti tambahan di sini jika diperlukan
+}
